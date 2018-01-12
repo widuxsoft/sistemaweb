d7db493ff03904db8c238d87675d284ef2131715
rafaelwidux <rafael@widux.com.ar>
2018-01-12 10:57:12 +0100

    a

---
+++ comerciales/Clases/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace comerciales.Clases
+{
+    public class Login
+    {
+            public string usuario { get; set; }
+            public string pass { get; set; }
+        
+    }
+}
---
+++ comerciales/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using comerciales.Clases;
+
+
+namespace comerciales.Controllers
+{
+    public class LoginController : Controller    
+    {
+        Login usuario = new Login();
+        // GET: Login
+        public ActionResult Index()
+        {
+            
+
+            return View(usuario);
+        }
+
+        public ActionResult Validar(Login model)
+        {
+            if (model.usuario.ToLower() =="adminenodje" && model.pass == "gestion17")
+            {
+               return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                ViewBag.mensaje_error = "Usuario y/o contraseña incorrectos";
+            }
+              return RedirectToAction("index");
+        }
+    }
+}
